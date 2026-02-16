fe0434a8cc6f891507f932862b04a720f6981663
claudiacarion <claudia.carion@gmail.com>
2026-02-16 10:59:48 +0100

    refactor data structure

--- data/data.js
+++ data/data.js
@@ -1,52 +1,61 @@
-export const infoArray = [
-  {
-    title: "Activities",
-    description: "Think Macau is only about gambling and casinos? Think again! This dynamic city is packed with adventure, culture, and outdoor fun. Walk historic streets, conquer sky-high thrills, explore museums, relax on beaches, or meet giant pandas in peaceful parks. From adrenaline rushes to family-friendly attractions, Macau proves that excitement comes in many forms—far beyond the gaming tables."
-  },
-    {
-    title: "Eat & Drink",
-    description: "Macau’s cuisine is a delicious fusion of Chinese, Portuguese, and Southeast Asian flavors, creating one of Asia’s most unique food cultures. From famous egg tarts and pork chop buns to rich curries and fresh seafood, every bite tells a story of heritage and creativity. Whether you’re dining by the sea or sampling street food, Macau promises a flavorful journey you won’t forget."
-  } 
-]
-
 export const activitiesArray = [
   {
-    name: "Macau Tower Convention & Entertainment Center",
-    description: "Ready for the ultimate thrill? Macau Tower isn’t just a landmark — it’s home to one of the highest commercial bungee jumps in the world! Feel your heart race as you leap from dizzying heights with stunning views of the Pearl River Delta below. Whether you choose skywalks, tower climbs, or simply sip a coffee with a view, Macau Tower delivers unforgettable moments that reach for the sky.",
-    image: "../public/images/tower.jpg",
-    link: "https://www.macautower.com.mo/"
+    title: "Activities",
+    description:
+      "Think Macau is only about gambling and casinos? Think again! This dynamic city is packed with adventure, culture, and outdoor fun. Walk historic streets, conquer sky-high thrills, explore museums, relax on beaches, or meet giant pandas in peaceful parks. From adrenaline rushes to family-friendly attractions, Macau proves that excitement comes in many forms—far beyond the gaming tables.",
+    activities: [
+      {
+        name: "Macau Tower Convention & Entertainment Center",
+        description:
+          "Ready for the ultimate thrill? Macau Tower isn’t just a landmark — it’s home to one of the highest commercial bungee jumps in the world! Feel your heart race as you leap from dizzying heights with stunning views of the Pearl River Delta below. Whether you choose skywalks, tower climbs, or simply sip a coffee with a view, Macau Tower delivers unforgettable moments that reach for the sky.",
+        image: "../public/images/tower.jpg",
+        link: "https://www.macautower.com.mo/",
+      },
+      {
+        name: "Macao Grand Prix Museum",
+        description:
+          "Start your engines at the home of the world’s most dangerous street circuit! The Macao Grand Prix Museum celebrates the city’s legendary race — where tight corners and narrow roads challenge drivers like nowhere else. Explore iconic cars, interactive exhibits, and the history behind this thrilling motorsport tradition. Whether you’re a racing fan or an adventure seeker, this museum brings high-speed culture to life.",
+        image: "../public/images/grandprix.jpeg",
+        link: "https://mgpm.macaotourism.gov.mo/en/",
+      },
+      {
+        name: "Giant Panda Pavillion",
+        description:
+          "Meet Macau’s fluffiest celebrities at the Giant Panda Pavilion! Nestled in lush greenery, this peaceful sanctuary lets you watch giant pandas munch bamboo, nap adorably, and charm visitors of all ages. It’s a refreshing escape from the city’s buzz and a perfect spot for family photos and quiet moments. Come say hello to these gentle giants and experience a softer, sweeter side of Macau.",
+        image: "../public/images/panda.jpg",
+        link: "https://www.macaotourism.gov.mo/en/shows-and-entertainment/family-fun/macao-giant-panda-pavilion",
+      },
+    ],
   },
-    {
-    name: "Macao Grand Prix Museum",
-    description: "Start your engines at the home of the world’s most dangerous street circuit! The Macao Grand Prix Museum celebrates the city’s legendary race — where tight corners and narrow roads challenge drivers like nowhere else. Explore iconic cars, interactive exhibits, and the history behind this thrilling motorsport tradition. Whether you’re a racing fan or an adventure seeker, this museum brings high-speed culture to life.",
-    image: "../public/images/grandprix.jpeg",
-    link: "https://mgpm.macaotourism.gov.mo/en/"
-  },
-    {
-    name: "Giant Panda Pavillion",
-    description: "Meet Macau’s fluffiest celebrities at the Giant Panda Pavilion! Nestled in lush greenery, this peaceful sanctuary lets you watch giant pandas munch bamboo, nap adorably, and charm visitors of all ages. It’s a refreshing escape from the city’s buzz and a perfect spot for family photos and quiet moments. Come say hello to these gentle giants and experience a softer, sweeter side of Macau.",
-    image: "../public/images/panda.jpg",
-    link: "https://www.macaotourism.gov.mo/en/shows-and-entertainment/family-fun/macao-giant-panda-pavilion"
-  }
-]
+];
 
 export const restaurantsArray = [
   {
-    name: "Lord Stow's Bakery",
-    description: "One bite and you’ll understand the hype. Lord Stow’s Bakery is the birthplace of Macau’s famous egg tart—crispy, creamy, and irresistibly golden. Fresh from the oven, these pastries are pure comfort wrapped in flaky perfection. Tucked away in charming Coloane Village, this cozy bakery is a must-stop for sweet lovers and souvenir hunters alike. Come hungry, leave happy.",
-    image: "../public/images/lordstows.jpg",
-    link: "https://www.lordstow.com/macau-outlets/"
+    title: "Eat & Drink",
+    description:
+      "Macau’s cuisine is a delicious fusion of Chinese, Portuguese, and Southeast Asian flavors, creating one of Asia’s most unique food cultures. From famous egg tarts and pork chop buns to rich curries and fresh seafood, every bite tells a story of heritage and creativity. Whether you’re dining by the sea or sampling street food, Macau promises a flavorful journey you won’t forget.",
+    restaurants: [
+      {
+        name: "Lord Stow's Bakery",
+        description:
+          "One bite and you’ll understand the hype. Lord Stow’s Bakery is the birthplace of Macau’s famous egg tart—crispy, creamy, and irresistibly golden. Fresh from the oven, these pastries are pure comfort wrapped in flaky perfection. Tucked away in charming Coloane Village, this cozy bakery is a must-stop for sweet lovers and souvenir hunters alike. Come hungry, leave happy.",
+        image: "../public/images/lordstows.jpg",
+        link: "https://www.lordstow.com/macau-outlets/",
+      },
+      {
+        name: "Fernando's Restaurant",
+        description:
+          "Sun, sand, and unforgettable flavors await at Fernando’s, where Portuguese classics meet the spirit of Macanese cuisine. Set beside the peaceful shores of Hac Sa Beach, this iconic restaurant serves hearty seafood, sizzling meats, and dishes inspired by Macau’s unique East-meets-West heritage. With ocean breezes, generous portions, and a laid-back vibe, Fernando’s offers more than a meal—it’s a true taste of Macau’s multicultural soul.",
+        image: "../public/images/fernandos.jpg",
+        link: "https://macaulifestyle.com/city-guide/fernandos-restaurant/",
+      },
+      {
+        name: "Tai Lei Loi Kei Porkchop Bun",
+        description:
+          "Crunchy, juicy, and wildly satisfying—the pork chop bun is Macau’s ultimate street-food hero. At Tai Lei Loi Kei, this simple sandwich becomes unforgettable: freshly fried pork, crusty bread, and bold flavor in every bite. Perfect for exploring on the go, it’s fast, filling, and full of local character. If you want to taste Macau like a local, start right here.",
+        image: "../public/images/porkchopbun.jpg",
+        link: "https://www.macaotourism.gov.mo/en/dining/specialty-foods/coffee-shops-and-noodle-shops/tai-lei-loi-kei-restaurant",
+      },
+    ],
   },
-    {
-    name: "Fernando's Restaurant",
-    description: "Sun, sand, and unforgettable flavors await at Fernando’s, where Portuguese classics meet the spirit of Macanese cuisine. Set beside the peaceful shores of Hac Sa Beach, this iconic restaurant serves hearty seafood, sizzling meats, and dishes inspired by Macau’s unique East-meets-West heritage. With ocean breezes, generous portions, and a laid-back vibe, Fernando’s offers more than a meal—it’s a true taste of Macau’s multicultural soul.",
-    image: "../public/images/fernandos.jpg",
-    link: "https://macaulifestyle.com/city-guide/fernandos-restaurant/"
-  },
-    {
-    name: "Tai Lei Loi Kei Porkchop Bun",
-    description: "Crunchy, juicy, and wildly satisfying—the pork chop bun is Macau’s ultimate street-food hero. At Tai Lei Loi Kei, this simple sandwich becomes unforgettable: freshly fried pork, crusty bread, and bold flavor in every bite. Perfect for exploring on the go, it’s fast, filling, and full of local character. If you want to taste Macau like a local, start right here.",
-    image: "../public/images/porkchopbun.jpg",
-    link: "https://www.macaotourism.gov.mo/en/dining/specialty-foods/coffee-shops-and-noodle-shops/tai-lei-loi-kei-restaurant"
-  }
-]
+];
