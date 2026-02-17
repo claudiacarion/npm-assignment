ab08412a0114112613dc66c489fc0950849f0254
claudiacarion <claudia.carion@gmail.com>
2026-02-17 11:31:39 +0100

    fix type

--- routes/activities.js
+++ routes/activities.js
@@ -1,10 +1,6 @@
 import express from "express";
 import * as path from "path";
-<<<<<<< HEAD
-import { activitiesArray } from '../data/data.js';
-=======
 import { activities } from '../data/data.js';
->>>>>>> feature/data
 
 const activitiesRouter = express.Router();
 const __dirname = path.resolve();
@@ -12,13 +8,8 @@ const __dirname = path.resolve();
 activitiesRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
     type: "subpage",
-<<<<<<< HEAD
-    title: "Explore",
-    array: activitiesArray,
-=======
     data: activities,
     title: activities.title,
->>>>>>> feature/data
     active: "activities"
   })
 })
