8768cb14e1a36df000b6c5dc6743b3387fdb101c
claudiacarion <claudia.carion@gmail.com>
2026-02-17 10:33:19 +0100

    change document title

--- routes/activities.js
+++ routes/activities.js
@@ -1,14 +1,15 @@
 import express from "express";
 import * as path from "path";
-import { activitiesArray } from '../data/data';
+// import { activitiesArray } from '../data/data';
 
 const activitiesRouter = express.Router();
 const __dirname = path.resolve();
 
 activitiesRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
+    type: "subpage",
     title: "Explore",
-    array: activitiesArray,
+    // array: activitiesArray,
     active: "activities"
   })
 })
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,16 +1,17 @@
 import express from "express";
 import * as path from "path";
-import { restaurantsArray } from '../data/data';
+// import { restaurantsArray } from "../data/data";
 
 const restaurantsRouter = express.Router();
 const __dirname = path.resolve();
 
 restaurantsRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
+    type: "subpage",
     title: "Eat & Drink",
-    array: restaurantsArray,
-    active: "restaurants"
-  })
-})
+    // array: restaurantsArray,
+    active: "restaurants",
+  });
+});
 
-export default restaurantsRouter
+export default restaurantsRouter;
