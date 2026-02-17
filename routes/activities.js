37bd07f3b248d0d5e2944b3eb0d039dca3a6e3ce
claudiacarion <claudia.carion@gmail.com>
2026-02-17 15:35:59 +0100

    main pages done, WIP subpages

--- routes/activities.js
+++ routes/activities.js
@@ -1,16 +1,42 @@
 import express from "express";
 import * as path from "path";
 import { activities } from '../data/data.js';
+import { slugify } from '../utils/slugify.js';
 
 const activitiesRouter = express.Router();
 const __dirname = path.resolve();
 
+const activitiesSlugs = {
+  ...activities,
+  items: activities.items.map(item => ({
+    ...item,
+    slug: slugify(item.name)
+  }))
+}
+
 activitiesRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
-    type: "subpage",
-    subpage: "activities",
-    data: activities,
+    type: "page",
+    page: "activities",
+    data: activitiesSlugs,
     title: activities.title,
+  })
+})
+
+activitiesRouter.get("/:slug", (req, res) => {
+  const { slug } = req.params;
+
+  const item = activities.items.find(item => slugify(item.name) === slug);
+
+  if (!item) {
+    return res.status(404).send("Page not found");
+  }
+
+  res.render(path.join(__dirname, "views/pages/page"), {
+    type: "subpage",
+    page: "activities",
+    data: activitiesSlugs,
+    title: item.name, 
   })
 })
 
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,19 +1,43 @@
 import express from "express";
 import * as path from "path";
 import { restaurants } from '../data/data.js';
+import { slugify } from '../utils/slugify.js';
 
 const restaurantsRouter = express.Router();
 const __dirname = path.resolve();
 
+const restaurantsSlugs = {
+  ...restaurants,
+  items: restaurants.items.map(item => ({
+    ...item,
+    slug: slugify(item.name)
+  }))
+}
+
 restaurantsRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
-    type: "subpage",
-    subpage: "restaurants",
-    data: restaurants,
+    type: "page",
+    page: "restaurants",
+    data: restaurantsSlugs,
     title: restaurants.title,
   })
 })
 
+restaurantsRouter.get("/:slug", (req, res) => {
+  const { slug } = req.params;
 
+  const item = restaurants.items.find(item => slugify(item.name) === slug);
+
+  if (!item) {
+    return res.status(404).send("Page not found");
+  }
+
+  res.render(path.join(__dirname, "views/pages/page"), {
+    type: "subpage",
+    page: "restaurants",
+    data: restaurantsSlugs,
+    title: item.name, 
+  })
+})
 
 export default restaurantsRouter
