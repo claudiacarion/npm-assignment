c00fd37d5d5a63265baf364087c2b5518fcbd7da
claudiacarion <claudia.carion@gmail.com>
2026-02-17 11:26:06 +0100

    refactor data file to object

--- routes/activities.js
+++ routes/activities.js
@@ -1,12 +1,15 @@
 import express from "express";
 import * as path from "path";
+import { activities } from '../data/data.js';
 
 const activitiesRouter = express.Router();
 const __dirname = path.resolve();
 
 activitiesRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
-    title: "Things To Do",
+    type: "subpage",
+    data: activities,
+    title: activities.title,
     active: "activities"
   })
 })
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,12 +1,15 @@
 import express from "express";
 import * as path from "path";
+import { restaurants } from '../data/data.js';
 
 const restaurantsRouter = express.Router();
 const __dirname = path.resolve();
 
 restaurantsRouter.get("/", (req, res) => {
   res.render(path.join(__dirname, "views/pages/page"), {
-    title: "Eat & Drink",
+    type: "subpage",
+    data: restaurants,
+    title: restaurants.title,
     active: "restaurants"
   })
 })
